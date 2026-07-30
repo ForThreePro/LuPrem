1478fa6fb27bea0c549c939f932e1d766d8ffa6f
YALLICO OWNER <yallico2024@gmail.com>
2026-07-30 00:06:31 -0500

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -3,21 +3,25 @@ var handler = async (m, { conn, args }) => {
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-conn.reply(m.chat, `⚡━━━━━━━━━━━━━━━⚡
-🔗 𝗘𝗡𝗟𝗔𝗖𝗘 𝗗𝗘𝗟 𝗚𝗥𝗨𝗣𝗢 🔗
-⚡━━━━━━━━━━━━━━━⚡
+conn.reply(m.chat, `🧡━━━━━━━━🧡
+   🔗 𝐄𝐍𝐋𝐀𝐂𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎 🔗
+🧡━━━━━━━━🧡
 
-╭─「 𝗜𝗡𝗙𝗢 𝗗𝗘𝗟 𝗦𝗜𝗦𝗧𝗘𝗠𝗔 」─╮
-│ 𝗘𝗡𝗟𝗔𝗖𝗘: ${link}
-│ 𝗘𝗦𝗧𝗔𝗗𝗢: 𝗔𝗰𝘁𝗶𝘃𝗼
-╰───────────────────────╯
+╭─「 🐾 𝐈𝐍𝐅𝐎 𝐆𝐀𝐑𝐅𝐈𝐄𝐋𝐃 」─╮
+│
+│ 🔗 𝗘𝗻𝗹𝗮𝗰𝗲 : ${link}
+│ 🍜 𝗘𝘀𝘁𝗮𝗱𝗼 : 𝗔𝗰𝘁𝗶𝘃𝗼
+│ 😼 𝗧𝗲𝗿𝗶𝘁𝗼𝗿𝗶𝗼 : 𝗦𝗲𝗴𝘂𝗿𝗼
+│
+╚━━━━━━━━━━╝
 
-> "𝗖𝗼𝗺𝗽𝗮𝗿𝘁𝗲 𝗰𝗼𝗻 𝗰𝘂𝗶𝗱𝗮𝗱𝗼"`, m, { detectLink: true })
+🐱 "𝗖𝗼𝗺𝗽𝗮𝗿𝘁𝗲 𝗰𝗼𝗻 𝗰𝘂𝗶𝗱𝗮𝗱𝗼 𝗼 𝘁𝗲 𝗾𝘂𝗶𝘁𝗼 𝗹𝗮 𝗹𝗮𝘀𝗮𝗴𝗻𝗮" 🧡
+`, m, { detectLink: true })
 
 }
 handler.help = ['link']
 handler.tags = ['grupo']
-handler.command = ['link', 'enlace']
+handler.command = ['link', 'enlace', 'grupolink']
 handler.group = true
 handler.botAdmin = true
 
