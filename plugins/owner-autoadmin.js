5dcd0948929b8987d44ca77343f10686d22760be
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 09:55:36 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,50 +1,50 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   if (isAdmin) return m.reply(`🧡━━━━━━━━🧡
-   😼 𝐋𝐔 𝐁𝐎𝐓 𝐏𝐑𝐄𝐌 😼
+   😼 **LU BOT PREM** 😼
 🧡━━━━━━━━🧡
 
-╭─「 ⚠️ 𝐀𝐕𝐈𝐒𝐎 」─╮
+╭─「 ⚠️ **AVISO** 」─╮
 │
-│ 🐾 𝗬𝗮 𝗲𝗿𝗲𝘀 𝗮𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝗱𝗼𝗿
-│ 🍜 𝗡𝗼 𝗽𝘂𝗲𝗱𝗼 𝗱𝗮𝗿𝘁𝗲 2 𝘃𝗲𝗰𝗲𝘀
+│ 🐾 **Ya eres administrador**
+│ 🍜 **No puedo darte 2 veces**
 │
 ╚━━━━━━━━━━╝
 
-😼 "𝗛𝗮𝘀𝘁𝗮 𝗚𝗮𝗿𝗳𝗶𝗲𝗹𝗱 𝘁𝗶𝗲𝗻𝗲 𝗹𝗶𝗺𝗶𝘁𝗲𝘀" 🧡
+😼 "**Hasta Garfield tiene limites**" 🧡
 `);
 
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
     await m.react('👑')
     m.reply(`🧡━━━━━━━━🧡
-   👑 𝐀𝐒𝐂𝐄𝐍𝐒𝐎 𝐂𝐎𝐍𝐂𝐄𝐃𝐈𝐃𝐎 👑
+   👑 **ASCENSO CONCEDIDO** 👑
 🧡━━━━━━━━🧡
 
-╭─「 🐾 𝐑𝐄𝐏𝐎𝐑𝐓𝐄 」─╮
+╭─「 🐾 **REPORTE** 」─╮
 │
-│ 😼 𝗨𝘀𝘂𝗮𝗿𝗶𝗼 : @${m.sender.split('@')[0]}
-│ 🍜 𝗡𝘂𝗲𝘃𝗼 𝗥𝗮𝗻𝗴𝗼 : 𝗔𝗱𝗺𝗶𝗻𝗶𝘀𝘁𝗿𝗮𝗱𝗼𝗿
-│ 🐾 𝗣𝗼𝗿 : 𝗦𝗶𝘀𝘁𝗲𝗺𝗮 𝗚𝗮𝗿𝗳𝗶𝗲𝗹𝗱
+│ 😼 **Usuario** : @${m.sender.split('@')[0]}
+│ 🍜 **Nuevo Rango** : **Administrador**
+│ 🐾 **Por** : **Sistema Garfield**
 │
 ╚━━━━━━━━━━╝
 
-🐱 "𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 𝗮𝗹 𝗰𝗹𝘂𝗯 𝗱𝗲 𝗹𝗮 𝗹𝗮𝘀𝗮𝗴𝗻𝗮" 🧡
+🐱 "**Bienvenido al club de la Lasaña**" 🧡
 `, null, { mentions: [m.sender] });
 
   } catch (e) {
     console.error(e)
     m.reply(`🧡━━━━━━━━🧡
-   ❌ 𝐄𝐑𝐎𝐑 𝐂𝐑𝐈𝐓𝐈𝐂𝐎 ❌
+   ❌ **ERROR CRITICO** ❌
 🧡━━━━━━━━🧡
 
-╭─「 😼 𝐃𝐄𝐓𝐀𝐋𝐄 」─╮
+╭─「 😼 **DETALLE** 」─╮
 │
-│ 🐾 𝗡𝗼 𝘀𝗲 𝗽𝘂𝗱𝗼 𝗱𝗮𝗿 𝗮𝗱𝗺𝗶𝗻
-│ 🍜 𝗩𝗲𝗿𝗶𝗳𝗶𝗰𝗮 𝗽𝗲𝗿𝗺𝗶𝘀𝗼𝘀 𝗱𝗲𝗹 𝗯𝗼𝘁
+│ 🐾 **No se pudo dar admin**
+│ 🍜 **Verifica permisos del bot**
 │
 ╚━━━━━━━━━━╝
 
-😼 "𝗛𝗮𝘀𝘁𝗮 𝗮 𝗺𝗶 𝗺𝗲 𝗱𝗮 𝗳𝗹𝗼𝗷𝗲𝗿𝗮 𝗮𝗿𝗲𝗴𝗹𝗮𝗿𝗹𝗼" 🧡
+😼 "**Hasta a mi me da flojera arreglarlo**" 🧡
 `);
   }
 };
